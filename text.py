39f40b9130153b5f3db7c2c7ece84e2a90a0c769
varshika15 <varshika15@gmail.com>
2018-01-29 20:39:23 +0530

    Update text.py

--- text.py
+++ text.py
@@ -9,14 +9,14 @@ Created on Sat Jan 27 15:49:34 2018
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
-# fix random seed for reproducibility
+
 numpy.random.seed(7)
-# load pima indians dataset
+# load data set
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-# split into input (X) and output (Y) variables
+# training/test data split
 X = dataset[:,0:8]
 Y = dataset[:,8]
-# create model
+# model building
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
@@ -25,6 +25,6 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
 model.fit(X, Y, epochs=150, batch_size=10)
-# evaluate the model
+# evaluating model
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
